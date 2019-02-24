15529e70bffa90529244b290e5c2d6d0ddeec231
David Zoltowski <david.m.zoltowski@gmail.com>
2019-02-23 20:11:30 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ssm-dm',
+      version='0.1.0',
+      description='State-space models for decision making',
+      authors='',
+      url='',
+      packages=['ssm-dm'],
+      install_requires=['numpy','scipy','matplotlib','ssm']
+      )
